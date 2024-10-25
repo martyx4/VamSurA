82cda9a1d21faa827bda4b073c4f0cda0f65036a
martyx4 <1689marty@gmail.com>
2024-10-25 13:52:03 +0900

    code1

---
+++ VamSurA/Assets/Undead Survivor/Codes/Bullet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Bullet : MonoBehaviour
+{
+	public float damage;
+	public int per;
+
+	Rigidbody2D right;
+
+	void Awake()
+	{
+		rigid = GetComponent<Rigidbody2D>();
+	}
+	public void Init(float damage, int per, Vector3 dir)
+	{
+		this.damage = damage;
+		this.per = per;
+
+		if (per > -1){
+			rigid.velocity = dir * 15f;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (??.CompareTag("Enemy")||per == -1)
+			return;
+		per--;
+
+		if (per == -1){
+			rigid.velocity = Vector2.zero;
+			gameObject.SetActive(false);
+		}
+	}
+}
