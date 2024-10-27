c68eaa31480b3ab8de7ec8be9aaf31667986676f
martyx4 <1689marty@gmail.com>
2024-10-27 22:04:57 +0900

    Progress to progrem 6+
    PLEASE READ README

--- VamSurA/Assets/Undead Survivor/Codes/Bullet.cs
+++ VamSurA/Assets/Undead Survivor/Codes/Bullet.cs
@@ -1,46 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Bullet : MonoBehaviour
 {
-	public float damage;
-	public int per;
+    public float damage;
+    public int per;
 
-	Rigidbody2D right;
-
-	void Awake()
-	{
-		rigid = GetComponent<Rigidbody2D>();
-	}
-	public void Init(float damage, int per, Vector3 dir)
-	{
-		this.damage = damage;
-		this.per = per;
-
-		if (per >= 0) {
-			rigid.velocity = dir * 15f;
-		}
-	}
-
-	void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (!collision.CompareTag("Enemy") || per == -100)
-			return;
-		per--;
-
-		if (per < 0) {
-			rigid.velocity = Vector2.zero;
-			gameObject.SetActive(false);
-		}
-	}
-
-	void OnTriggerExit2D(Collider2D collision)
-	{
-		if (!collision.CompareTag("Area") || per == -100)
-			return;
-
-		gameObject.SetActive(false);
-	}
+    public void Init(float damage, int per)//ÃÊ±âÈ­
+    {
+        this.damage = damage;
+        this.per = per;
+    }
 }
--- VamSurA/Assets/Undead Survivor/Codes/Reposition.cs
+++ VamSurA/Assets/Undead Survivor/Codes/Reposition.cs
@@ -4,44 +4,46 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
-	Collider2D coll;
+    Collider2D coll;
 
-	void Awake()
-	{
-		coll = GetComponent<Collider2D>();
-	}
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
 
-	void OnTriggerExit2D(Collider2D collision)
-	{
-		if  (!collision.CompareTag("Area"))
-			return;
-		
-		Vector3 playerPos = GameManager.instance.player.transform.position;
-		Vector3 myPos = transform.position;
-		
-		switch (transform.tag) {
-			case "Ground":
-				float diffX = playerPos.x - myPos.x;
-				float diffY = plaerPos.y - myPos.y;
-				float dirX = diffX < 0 ? -1 : 1;
-				float dirY = diffY < 0 ? -1 : 1;
-				diffX = Mathf.Abs(diffX);
-				diffY = Mathf.Abs(diffY);
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area")) //Area 태그가 아니라면(!) 실행 안함(return)
+            return;
+        
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float diffX = Mathf.Abs(playerPos.x - myPos.x);//거리 구하기, Abs: 절댓값(음수 방지)
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
 
-				if (diffX > diffY) {
-					transform.Translate(Vector3.right * dirX * 40);
-				}
-				else if (diffX < diffY) {
-					transform.Translate(Vector3.up * dirY * 40);
-				}
-				break;
-			case "Enemy":
-				if (coll.enabled) {
-					Vector3 dist = playerPos - myPos;
-					Vector3 ran = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
-					transform.Translate(ran + dist * 2);
-				}
-				break;
-		}
-	}
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1; //조건이 어떤 상태라면? (참 시 주는 값 : 거짓 시 주는 값)
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag) //태그가 어떤 상태인지(태그가 뭔지)
+        {
+            case "Ground":
+                if (diffX > diffY)// X축 거리차이가 Y축 거리차이보다 크다
+                {
+                    transform.Translate(Vector3.right * dirX * 40);//지정된 값 만큼 현재위치에서 이동, 이동량 대입
+                }
+                else if (diffX < diffY)// Y축 거리차이가 X축 거리차이보다 크다
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+
+            case "Enemy":
+                if (coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-1f, 1f), Random.Range(-3f, 3f), 0f));//플레이어 맞은편에서 등장
+                }
+                break;
+        }
+    }
 }
